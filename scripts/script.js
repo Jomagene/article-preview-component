bbcaefcd9c1c9b985d0cab4ee83d0f42a3247d63
Jomagene <semjomagene@gmail.com>
2024-08-23 21:57:39 +0200

    Implement link sharing logic

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,5 @@
+// Toggle social tooltip
+
 let shareIcons = document.querySelectorAll(".share-icon-container");
 let shareIcon = shareIcons[0];
 let tooltip = document.getElementById("tooltip");
@@ -10,3 +12,18 @@ function toggleTooltip() {
 for (let icon of shareIcons) {
   icon.addEventListener("click", toggleTooltip);
 }
+
+// Link sharing implementation
+
+const link = encodeURI(window.location.href);
+const msg = encodeURIComponent("Hey! I found this drawer so interesting");
+const title = encodeURIComponent(document.querySelector("h1").textContent);
+
+const socials = document.querySelector(".social-links").children;
+const facebook = socials[1];
+const twitter = socials[2];
+const pinterest = socials[3];
+
+facebook.href = `http://www.facebook.com/share.php?u=${link}`;
+twitter.href = `http://www.twitter.com/share.php?&url=${link}&text=${msg}`;
+pinterest.href = `http://www.pinterest.com/share.php?u=${link}`;
