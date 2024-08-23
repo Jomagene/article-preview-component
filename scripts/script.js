dc47eed97d582575b209e636884cf6f3079e8f8e
Jomagene <semjomagene@gmail.com>
2024-08-23 19:31:50 +0200

    hidde the tooltip's carret on mobile

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,10 @@
 let shareIcons = document.querySelectorAll(".share-icon-container");
+let shareIcon = shareIcons[0];
 let tooltip = document.getElementById("tooltip");
 
 function toggleTooltip() {
   tooltip.classList.toggle("hidden");
+  shareIcon.classList.toggle("active");
 }
 
 for (let icon of shareIcons) {
